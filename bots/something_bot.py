e01d57a6f88fcade2d73628d0b881ead1af9ad96
angyettii <angchi856@gmail.com>
2025-02-15 16:20:43 -0500

    changes

--- bots/something_bot.py
+++ bots/something_bot.py
@@ -35,40 +35,43 @@ class BotPlayer(Player):
 
 
         # like attack bot, attack all buildings
-        # enemy_buildings = rc.get_buildings(enemy)
-        # for building in enemy_buildings:
-        #     if building.type == BuildingType.MAIN_CASTLE:
-        #         enemy_castle_id = rc.get_id_from_building(building)[1]
-        #         enemy_castle = rc.get_building_from_id(enemy_castle_id)
-        #         if enemy_castle is None: 
-        #             continue
-        #         # loop through all the units
-        #         for unit_id in my_units:
+        enemy_buildings = rc.get_buildings(enemy)
+        for building in enemy_buildings:
+            if building.type == BuildingType.MAIN_CASTLE:
+                enemy_castle_id = rc.get_id_from_building(building)[1]
+                enemy_castle = rc.get_building_from_id(enemy_castle_id)
+                if enemy_castle is None: 
+                    continue
+                # loop through all the units
+                for unit_id in my_units:
 
-        #             # if castle still stands and can attack castle, attack castle
-        #             if enemy_castle_id in rc.get_building_ids(enemy) and rc.can_unit_attack_building(unit_id, enemy_castle_id):
-        #                 rc.unit_attack_building(unit_id, enemy_castle_id)
+                    # if castle still stands and can attack castle, attack castle
+                    if enemy_castle_id in rc.get_building_ids(enemy) and rc.can_unit_attack_building(unit_id, enemy_castle_id):
+                        rc.unit_attack_building(unit_id, enemy_castle_id)
 
-        #             # if can move towards castle, move towards castle
-        #             unit = rc.get_unit_from_id(unit_id)
-        #             if unit is None:
-        #                 return
+                    # if can move towards castle, move towards castle
+                    unit = rc.get_unit_from_id(unit_id)
+                    if unit is None:
+                        return
                     
-        #             possible_move_dirs = rc.unit_possible_move_directions(unit_id)
-        #             possible_move_dirs.sort(key= lambda dir: rc.get_chebyshev_distance(*rc.new_location(unit.x, unit.y, dir), enemy_castle.x, enemy_castle.y))
+                    possible_move_dirs = rc.unit_possible_move_directions(unit_id)
+                    possible_move_dirs.sort(key= lambda dir: rc.get_chebyshev_distance(*rc.new_location(unit.x, unit.y, dir), enemy_castle.x, enemy_castle.y))
 
-        #             best_dir = possible_move_dirs[0] if len(possible_move_dirs) > 0 else Direction.STAY #least chebyshev dist direction
+                    best_dir = possible_move_dirs[0] if len(possible_move_dirs) > 0 else Direction.STAY #least chebyshev dist direction
 
-        #             if rc.can_move_unit_in_direction(unit_id, best_dir):
-        #                 rc.move_unit_in_direction(unit_id, best_dir)
+                    if rc.can_move_unit_in_direction(unit_id, best_dir):
+                        rc.move_unit_in_direction(unit_id, best_dir)
         
         
         enemy_unit_id = -1
-
+    
         # similar, attack all units
         enemy_units = rc.get_units(enemy)
+        rc.unit_attack_unit(unit_id, enemy_unit_id)
+        
         for e_unit in enemy_units:
             enemy_unit_id = rc.get_id_from_unit(e_unit)[0]
+            
             enemy_unit = rc.get_unit_from_id(enemy_unit_id)
             
             if enemy_unit is None: 
@@ -77,8 +80,7 @@ class BotPlayer(Player):
             for unit_id in my_units:
 
                 # if castle still stands and can attack castle, attack castle
-                if enemy_unit_id in rc.get_unit_ids(enemy) and rc.can_unit_attack_unit(unit_id, enemy_unit):
-                    
+                if rc.can_unit_attack_unit(unit_id, enemy_unit):
                     rc.unit_attack_unit(unit_id, enemy_unit_id)
 
                 # if can move towards castle, move towards castle
@@ -89,7 +91,7 @@ class BotPlayer(Player):
                 possible_move_dirs = rc.unit_possible_move_directions(unit_id)
                 possible_move_dirs.sort(key= lambda dir: rc.get_chebyshev_distance(*rc.new_location(unit.x, unit.y, dir), enemy_unit.x, enemy_unit.y))
 
-                best_dir = possible_move_dirs[0] if len(possible_move_dirs) > 0 else Direction.STAY #least chebyshev dist direction
+                best_dir = possible_move_dirs[0] if len(possible_move_dirs) >= 0 else Direction.STAY #least chebyshev dist direction
 
                 if rc.can_move_unit_in_direction(unit_id, best_dir):
                     rc.move_unit_in_direction(unit_id, best_dir)
